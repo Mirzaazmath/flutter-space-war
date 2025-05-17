3b6d91eae39134a40af1d1a004cadc4ba5d605d1
Mirzaazmath <azmath9603399270@gmail.com>
2025-05-17 13:07:25 +0530

    added create player and start game method in mygame

--- lib/my_game.dart
+++ lib/my_game.dart
@@ -3,18 +3,32 @@ import 'dart:async';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
+import 'components/player.dart';
+
 class MyGame extends FlameGame {
+  late Player player;
+
   /// Here we are configuring things while loading game
   @override
   FutureOr<void> onLoad() async {
-    /// here we are setting our device in full screenMode
+    /// Here we are setting our device in full screenMode
     await Flame.device.fullScreen();
 
-    /// here we are setting our device in  portraitMode
+    /// Here we are setting our device in  portraitMode
     await Flame.device.setPortrait();
+   /// Here we are calling the  startGame method
+    startGame();
 
     return super.onLoad();
   }
-
-
+/// Here we are starting the game
+  void startGame() {
+    /// Here we are calling the _createPLayer to create a player
+    _createPLayer();
+  }
+/// Here we are creating the player and adding that player into our game
+  void _createPLayer() {
+    player = Player();
+    add(player);
+  }
 }
